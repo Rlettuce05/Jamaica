129e366cd8e3bc4cd47ad86a7c343954e385ed76
syunsuke okamura <syunrobin@gmail.com>
2021-03-04 21:55:03 +0900

    update : add window

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import PySimpleGUI as ps
+import PySimpleGUI as psg
 import random
+
+from PySimpleGUI.PySimpleGUI import main
 
 for i in range(7):
     DiceRoll = random.randint(1,6)
+
+Main = [[psg.test("test")],
+        [psg.test(DiceRoll)],
+        [psg.Button("start")]]
     
+window = psg.Window("main window",Main)
