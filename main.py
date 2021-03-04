d08a6e900030183fed0c7fcb4fdd34f27561105a
syunsuke okamura <syunrobin@gmail.com>
2021-03-04 21:57:09 +0900

    fix : test => text

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from PySimpleGUI.PySimpleGUI import main
 for i in range(7):
     DiceRoll = random.randint(1,6)
 
-Main = [[psg.test("test")],
-        [psg.test(DiceRoll)],
+Main = [[psg.Text("test")],
+        [psg.Text(DiceRoll)],
         [psg.Button("start")]]
     
 window = psg.Window("main window",Main)
